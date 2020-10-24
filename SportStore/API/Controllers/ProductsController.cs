8f472782707a306832c2803763ec63adba984aed
rinat-gk <rinatgko1@gmail.com>
2020-10-24 18:51:19 +0400

    Add Entities, Entity Framework and migrations

---
+++ SportStore/API/Controllers/ProductsController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+
+        public string GetProducts()
+        {
+            return "this will be a list of products";
+        }
+
+        [HttpGet("{id}")]
+
+        public string GetProduct(int id)
+        {
+            return "this will be a single product";
+        }
+    }
+}
